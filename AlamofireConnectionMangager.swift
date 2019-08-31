36b13f9efb84f1c6abbb3497595b397ed4297af4
anup gupta <im.anupgupta03@gmail.com>
2019-08-31 12:12:59 +0530

    update

--- AlamofireConnectionMangager.swift
+++ AlamofireConnectionMangager.swift
@@ -6,17 +6,12 @@
 //  Copyright © 2018 GeekGuns. All rights reserved.
 //
 
+
 import Foundation
 import Alamofire
 
 class AlamofireConnectionMangager {
-    
-    
-//     static let sharedInstance = AlamofireConnectionMangager()
-    
-//    private init() {
-        
-//     }
+  
     private static var _instance: AlamofireConnectionMangager?;
     
     private init() {
@@ -33,45 +28,81 @@ class AlamofireConnectionMangager {
     func getDataFromServer( url: String , param : NSDictionary, success: @escaping (NSDictionary) -> () , failure: @escaping (Error?) -> () ) {
         
         
-    Alamofire.request(url , method: .post, parameters: param as? [String: Any] , encoding: URLEncoding.default, headers: nil).responseJSON
+        Alamofire.request(url , method: .get, parameters: param as? [String: Any] , encoding: URLEncoding.default, headers: nil).responseJSON
             {
                 (response:DataResponse<Any>) in
                 
-                print("response.request :", response.request as Any)
-                print("response.response :", response.response as Any)
-                print("response.result:", response.result as Any)
-                print("response.value: ", response.value as Any)
-                print("response.error:", response.error as Any)
+                //                print("response.request :", response.request as Any)
+                //                print("response.response :", response.response as Any)
+                //                print("response.result:", response.result as Any)
+                //                print("response.value: ", response.value as Any)
+                //                print("response.error:", response.error as Any)
                 
+                //                let StatusCode = response.response?.statusCode
                 
-                
-                 if (response.error != nil) {
+                if (response.error != nil) {
                     failure(response.error);
                 }
                 else if (response.value != nil) {
                     success(response.value as! NSDictionary)
                 }
-//                 let StatusCode = response.response?.statusCode
-//                 if response.result.isSuccess && StatusCode == 200{
-                    
-//                     print("post data")
-//                     let dic : NSDictionary = response.value as! NSDictionary
-
-//                     success(dic)
-                    
-//                 }
-//                 else{
-//                     print(response.error?.localizedDescription as Any)
-//                     let messageStr : String = (response.error?.localizedDescription)!
-//                     print("error message:::",messageStr)
-//                     failure(true)
-//                 }
+                
                 
         }
         
     }
     
+    func postDataToServer( url: String , param : NSDictionary, success: @escaping (NSDictionary) -> () , failure: @escaping (Error?) -> () ) {
+        
+        Alamofire.request(url , method: .post, parameters: param as? [String: Any] , encoding: URLEncoding.default, headers: nil).responseJSON
+            {
+                (response:DataResponse<Any>) in
+                //                print("response.request :", response.request as Any)
+                //                print("response.response :", response.response as Any)
+                //                print("response.result:", response.result as Any)
+                //                print("response.value: ", response.value as Any)
+                //                print("response.error:", response.error as Any)
+                
+                if (response.error != nil) {
+                    failure(response.error);
+                }
+                else if (response.value != nil) {
+                    success(response.value as! NSDictionary)
+                }
+                
+                
+        }
+        
+    }
+    
+    func downloadImagesFromServer(url: String , success: @escaping (Data) -> () , failure: @escaping (Error?) -> ()){
+        
+        // Use Alamofire to download the image
+        Alamofire.request(url).responseData { (response) in
+            if response.error == nil {
+                print(response.result)
+                
+                // Show the downloaded image:
+                if let data = response.data {
+                    // self.downloadImage.image = UIImage(data: data)
+                    success(data)
+                    
+                }
+            }
+            else{
+                failure(response.error)
+                
+            }
+        }
+        
+    }
+    
+    
+    
 }
+
+
+
 
 
 // This is Use Description
